6384c3f9cd7e9b5a7539d00638f87b99ddee1880
daniel <dani.ajala@yahoo.com>
2021-11-18 19:53:42 -0300

    api-post

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,5 +1,5 @@
 
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 
 hoteis = [
@@ -38,17 +38,35 @@ class Hotel(Resource):
         for hotel in hoteis:
             if hotel["hotel_id"] == hotel_id:
                 return hotel
+        return {"message":"hotel nao existe"}
            
 
 
-        
-            
-           
-       
-            
+    def post(self,hotel_id):
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument('nome')
+        argumentos.add_argument('diaria')
+        argumentos.add_argument('estrelas')
+        argumentos.add_argument('cidade')
 
-    def post(self):
-        pass
+        dados = argumentos.parse_args()
+
+        novo_hotel = {
+             'hotel_id':hotel_id,
+            'nome':dados['nome'],
+            'diaria':dados[ 'diaria'],
+            'estrelas':dados['estrelas'],
+            'cidade':dados['cidade']
+
+
+        }
+        hoteis.append(novo_hotel)
+
+  
+
+    
+
+
 
 
     def put(self):
